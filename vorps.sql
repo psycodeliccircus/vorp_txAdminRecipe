dbfa2f25b56a3e5cdeb2c572fd034bb0a6344cef
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2022-02-03 05:19:41 +0300

    New Version 1.1.0
    Some vorps scripts added to recipe.
    Deleted syn files.
    Changed all scripts default lang to EN
    Update .sql file

--- vorps.sql
+++ vorps.sql
@@ -75,3 +75,126 @@ COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 ROW_FORMAT=DYNAMIC
 ;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for outfits
+-- ----------------------------
+DROP TABLE IF EXISTS `outfits`;
+CREATE TABLE `outfits`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `charidentifier` int(11) NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `comps` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for stables
+-- ----------------------------
+DROP TABLE IF EXISTS `stables`;
+CREATE TABLE `stables`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `charidentifier` int(11) NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `modelname` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `type` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `xp` int(11) NULL DEFAULT 0,
+  `injured` int(11) NULL DEFAULT 0,
+  `gear` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `isDefault` int(11) NOT NULL DEFAULT 0,
+  `inventory` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `horse_complements`;
+CREATE TABLE `horse_complements`  (
+  `identifier` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `charidentifier` int(11) NOT NULL,
+  `complements` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  UNIQUE INDEX `identifier`(`identifier`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE `banneds` (
+  `b_id` int(11) NOT NULL,
+  `b_steam` varchar(100) NOT NULL,
+  `b_license` varchar(255) DEFAULT NULL,
+  `b_discord` varchar(100) DEFAULT NULL,
+  `b_reason` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `b_banned` varchar(100) NOT NULL,
+  `b_unban` varchar(100) NOT NULL,
+  `b_permanent` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+
+ALTER TABLE `banneds`
+  ADD PRIMARY KEY (`b_id`) USING BTREE;
+
+
+ALTER TABLE `banneds`
+  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for housing
+-- ----------------------------
+DROP TABLE IF EXISTS `housing`;
+CREATE TABLE `housing`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `charidentifier` int(11) NOT NULL,
+  `inventory` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `furniture` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `open` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 34307 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE `rooms`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `interiorId` int(11) NOT NULL,
+  `inventory` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `identifier` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `charidentifier` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- Insert data for items
+INSERT INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES
+('consumable_coffee', 'Cafe', 5, 1, 'item_standard', 1),
+('consumable_haycube', 'Heno', 50, 1, 'item_standard', 1),
+('consumable_kidneybeans_can', 'Lata de alubias', 5, 1, 'item_standard', 1),
+('consumable_medicine', 'Medicina', 3, 1, 'item_standard', 1),
+('consumable_peach', 'Melocoton', 5, 1, 'item_standard', 1),
+('consumable_salmon_can', 'Lata de Salmon', 5, 1, 'item_standard', 1),
+('ammo_bullet_pistol', 'Pistol Ammo', 10, 1, 'item_standard', 1),
+('ammo_bullet_revolver', 'Revolver Ammo', 10, 1, 'item_standard', 1),
+('ammo_bullet_rifle', 'Rifle Ammo', 1, 1, 'item_standard', 1),
+('ammo_bullet_varmint', 'Varmint Ammo', 1, 1, 'item_standard', 1),
+('ammo_shotgun', 'Shotgun Shells', 10, 1, 'item_standard', 1),
+('ammo_bullet_repeater', 'Repeater Ammo', 10, 1, 'item_standard', 1),
+('wood', 'Wood', 50, 1, 'item_standard', 0),
+('wooden_boards', 'Wooden Boards', 25, 1, 'item_standard', 0);
